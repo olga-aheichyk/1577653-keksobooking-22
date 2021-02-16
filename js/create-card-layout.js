40b541f8d2767395cfad2389812583bc026f8ff4
olga.aheichyk <olga.aheichyk@gmail.com>
2021-02-16 11:13:46 +0300

    fix: edit feature textContent, refactor code

--- js/create-card-layout.js
+++ js/create-card-layout.js
@@ -1,53 +1,32 @@
-import {CARDS_COUNT, makeCards} from './make-cards.js';
-const mocks = makeCards(CARDS_COUNT);
-//console.log(mocks);
-
-const mapCanvas = document.querySelector('.map__canvas');
-
-
-
-
+/**
+ * Функция создания DOM-элемента тестовой карточки объявления из объекта
+ * @param {object} — объект тестовой карточки объявления
+ * @return {object} — DOM-элемент тестовой карточки объявления
+ */
 const createCardLayout = function(object) {
   const cardTemplate = document.querySelector('#card').content;
   const cardTemplateArticle = cardTemplate.querySelector('article');
 
   const cardArticle = cardTemplateArticle.cloneNode(true);
-  const articleTitle = cardArticle.querySelector('.popup__title');
-  const articleAddress = cardArticle.querySelector('.popup__text--address');
-  const articlePrice = cardArticle.querySelector('.popup__text--price');
-  const articleType = cardArticle.querySelector('.popup__type');
-  const articleText = cardArticle.querySelector('.popup__text--capacity');
-  const articleTime = cardArticle.querySelector('.popup__text--time');
+
+  cardArticle.querySelector('.popup__avatar').src = object.avatar;
+  cardArticle.querySelector('.popup__title').textContent = object.offer.title;
+  cardArticle.querySelector('.popup__text--address').textContent = object.offer.address;
+  cardArticle.querySelector('.popup__text--price').textContent = `${object.offer.price} ₽/ночь`;
+  cardArticle.querySelector('.popup__type').textContent = Object.values(object.offer.type);
+  cardArticle.querySelector('.popup__text--capacity').textContent = `${object.offer.rooms} комнаты для ${object.offer.guests} гостей.`;
+  cardArticle.querySelector('.popup__text--time').textContent = `Заезд после ${object.offer.checkin}, выезд до ${object.offer.checkout}.`;
+  cardArticle.querySelector('.popup__description').textContent = object.offer.description;
+
   const articleFeatures = cardArticle.querySelector('.popup__features');
-  const articleDescription = cardArticle.querySelector('.popup__description');
+  articleFeatures.textContent = '';
+  let featuresHtmlLayout = object.offer.features
+    .map(item => `<li class="popup__feature popup__feature--${item}"></li>`)
+    .join('\n');
+  articleFeatures.innerHTML = featuresHtmlLayout;
+
   const articlePhotos = cardArticle.querySelector('.popup__photos');
   const articlePhoto = cardArticle.querySelector('.popup__photo');
-  const articleAvatar = cardArticle.querySelector('.popup__avatar');
-
-  articleTitle.textContent = object.offer.title;
-  articleAddress.textContent = object.offer.address;
-  articlePrice.textContent = `${object.offer.price} ₽/ночь`
-
-  let typeName;
-  switch (object.offer.type) {
-    case 'flat' : typeName = 'Квартира';
-      break;
-
-    case 'bungalow' : typeName = 'Бунгало';
-      break;
-
-    case 'house' : typeName = 'Дом';
-      break;
-
-    case 'palace' : typeName = 'Дворец';
-      break;
-  }
-
-  articleType.textContent = typeName;
-  articleText.textContent = `${object.offer.rooms} комнаты для ${object.offer.guests} гостей.`;
-  articleTime.textContent = `Заезд после ${object.offer.checkin}, выезд до ${object.offer.checkout}.`;
-  articleFeatures.textContent = `${object.offer.features}`;
-  articleDescription.textContent = `${object.offer.description}`;
   articlePhoto.src = object.offer.photos[0];
 
   if (object.offer.photos.length > 1) {
@@ -57,14 +36,8 @@ const createCardLayout = function(object) {
       articlePhotos.appendChild(nextArticlePhoto);
     }
   }
-
-  articleAvatar.src = object.avatar;
-
   return cardArticle;
-
 }
 
-const mock = createCardLayout(mocks[0]);
+export {createCardLayout};
 
-
-mapCanvas.appendChild(mock);
--- js/main.js
+++ js/main.js
@@ -1,4 +1,14 @@
-import  './create-card-layout.js';
+import {CARDS_COUNT, makeCards} from './make-cards.js';
+import {createCardLayout} from './create-card-layout.js';
+
+const mapCanvas = document.querySelector('.map__canvas');
+
+const mockObjects = makeCards(CARDS_COUNT);
+
+const mockCard = createCardLayout(mockObjects[0]);
+
+mapCanvas.appendChild(mockCard);
+
 
 
 
